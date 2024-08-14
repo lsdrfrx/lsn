8551f4378a74c88cd98ba71361464baf8f93950a
Christian Guetnga <lsdrfrx@gmail.com>
2024-08-14 13:42:23 +0300

    feat: add colored output
    fix: add isFile call to isExecutable procedure

--- main.nim
+++ main.nim
@@ -1,11 +1,8 @@
 import os
 import tables
 import strformat
+import terminal
 from icons import Icons
-
-proc isExecutable(path: string): bool =
-  var permissions = path.getFilePermissions()
-  return permissions.contains(fpUserExec)
 
 proc isDirectory(path: string): bool =
   return path.dirExists()
@@ -13,23 +10,31 @@ proc isDirectory(path: string): bool =
 proc isFile(path: string): bool =
   return path.fileExists()
 
-proc processEntry(path: string): string =
+proc isExecutable(path: string): bool =
+  var permissions = path.getFilePermissions()
+  return permissions.contains(fpUserExec) and path.isFile()
+
+proc processEntry(path: string) =
   var entryParts = splitFile(path)
   var icon: string
+  var color: ForegroundColor
 
   if path.isFile():
+    color = fgDefault
     icon = Icons["ext"].getOrDefault(entryParts.ext, Icons["other"]["plainFile"])
   elif path.isDirectory():
+    color = fgBlue
     icon = Icons["directories"].getOrDefault(entryParts.name, Icons["other"]["directory"])
   
   if path.isExecutable():
-    # TODO: Add green color to executable files
-    icon = Icons["other"]["executable"]
+    color = fgGreen
+    if icon == Icons["other"]["plainFile"]:
+      icon = Icons["other"]["executable"]
 
-  return "{icon} {entryParts.name}{entryParts.ext}".fmt()
+  stdout.styledWrite(color, "{icon} {entryParts.name}{entryParts.ext}  ".fmt())
 
 
 var currentDir = os.getCurrentDir()
 
 for entry in os.walkDir(currentDir):
-   echo processEntry(entry.path)
+   processEntry(entry.path)
