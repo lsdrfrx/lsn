27febf7d50fa322e70e91e0b62a58c48c1dcd97e
Christian Guetnga <lsdrfrx@gmail.com>
2024-08-14 14:17:05 +0300

    feat: add command line arguments parsing

--- main.nim
+++ main.nim
@@ -2,7 +2,17 @@ import os
 import tables
 import strformat
 import terminal
+import parseopt
 from icons import Icons
+
+var p = initOptParser(commandLineParams())
+
+var directory: string
+
+var showHidden = false          # -a
+var showLine = true             # -l
+var showMeta = false            # -m
+var showDirsOnly = false        # -d
 
 proc isDirectory(path: string): bool =
   return path.dirExists()
@@ -19,6 +29,12 @@ proc processEntry(path: string) =
   var icon: string
   var color: ForegroundColor
 
+  if not showHidden:
+    if entryParts.name[0] == '.': return
+
+  if showDirsOnly:
+    if path.isFile(): return
+
   if path.isFile():
     color = fgDefault
     icon = Icons["ext"].getOrDefault(entryParts.ext, Icons["other"]["plainFile"])
@@ -31,10 +47,29 @@ proc processEntry(path: string) =
     if icon == Icons["other"]["plainFile"]:
       icon = Icons["other"]["executable"]
 
-  stdout.styledWrite(color, "{icon} {entryParts.name}{entryParts.ext}  ".fmt())
+  if showLine:
+    stdout.styledWriteLine(color, "{icon} {entryParts.name}{entryParts.ext}".fmt())
+  else:
+    stdout.styledWrite(color, "{icon} {entryParts.name}{entryParts.ext}\t".fmt())
 
 
-var currentDir = os.getCurrentDir()
+while true:
+  p.next()
+  case p.kind
+  of cmdEnd: break
+  of cmdShortOption, cmdLongOption:
+    case p.key:
+    of "a": showHidden = true
+    of "l": showLine = false
+    of "m": showMeta = true
+    of "d": showDirsOnly = true
+  of cmdArgument:
+    directory = p.key
 
-for entry in os.walkDir(currentDir):
-   processEntry(entry.path)
+if directory == "":
+  directory = os.getCurrentDir()
+
+for entry in os.walkDir(directory):
+  processEntry(entry.path)
+
+if not showLine: echo ""
