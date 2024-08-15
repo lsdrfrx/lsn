03f90f98b2d02f81b0a7a8a10e725e1f2164b7e1
Christian Guetnga <lsdrfrx@gmail.com>
2024-08-15 10:50:03 +0300

    refactor: move cmdline arguments parsing into separate procedure
    refactor: showLine -> showList

--- main.nim
+++ main.nim
@@ -5,14 +5,28 @@ import terminal
 import parseopt
 from icons import Icons
 
-var p = initOptParser(commandLineParams())
-
 var directory: string
 
 var showHidden = false          # -a
-var showLine = true             # -l
+var showList = true             # -l
 var showMeta = false            # -m
 var showDirsOnly = false        # -d
+
+proc parseCommandLineArgs() =
+  var p = initOptParser(commandLineParams())
+
+  while true:
+    p.next()
+    case p.kind
+    of cmdEnd: break
+    of cmdShortOption, cmdLongOption:
+      case p.key:
+      of "a": showHidden = true
+      of "l": showList = false
+      of "m": showMeta = true
+      of "d": showDirsOnly = true
+    of cmdArgument:
+      directory = p.key
 
 proc isDirectory(path: string): bool =
   return path.dirExists()
@@ -47,29 +61,18 @@ proc processEntry(path: string) =
     if icon == Icons["other"]["plainFile"]:
       icon = Icons["other"]["executable"]
 
-  if showLine:
+  if showList:
     stdout.styledWriteLine(color, "{icon} {entryParts.name}{entryParts.ext}".fmt())
   else:
     stdout.styledWrite(color, "{icon} {entryParts.name}{entryParts.ext}\t".fmt())
 
+if isMainModule:
+  parseCommandLineArgs()
 
-while true:
-  p.next()
-  case p.kind
-  of cmdEnd: break
-  of cmdShortOption, cmdLongOption:
-    case p.key:
-    of "a": showHidden = true
-    of "l": showLine = false
-    of "m": showMeta = true
-    of "d": showDirsOnly = true
-  of cmdArgument:
-    directory = p.key
+  if directory == "":
+    directory = os.getCurrentDir()
 
-if directory == "":
-  directory = os.getCurrentDir()
+  for entry in os.walkDir(directory):
+    processEntry(entry.path)
 
-for entry in os.walkDir(directory):
-  processEntry(entry.path)
-
-if not showLine: echo ""
+  if not showList: echo ""
